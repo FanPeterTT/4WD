eb0a8b745773c687ac27af75f2fb6d1883e7e51d
PeterFan <18335909110@163.com>
2020-12-16 22:34:03 +0800

    pro

--- DemoStm32f103/MyCode/Pro.c
+++ DemoStm32f103/MyCode/Pro.c
@@ -22,15 +22,31 @@ int pro_pktSend(char *buf, int bufLen, char cmd, char *para, int paraLen)
         buf[PRO_PARA + i] = para[i];
         sum += para[i];
     }
-    buf[PRO_CHECKSUM + paraLen] = sum + buf[PRO_SRC] + buf[PRO_DST] + buf[PRO_LEN] + buf[PRO_CMD];
-    buf[PRO_TAIL + paraLen] = 0xFE;
+    buf[PRO_CHECKSUM + paraLen - 1] = sum + buf[PRO_SRC] + buf[PRO_DST] + buf[PRO_LEN] + buf[PRO_CMD];
+    buf[PRO_TAIL + paraLen - 1] = 0xFE;
 
     return PRO_TAIL + paraLen + 1;
 }
 
-int pro_pktRes(char *buf)
+int pro_pktRes(char *buf, int bufLen, char cmd, char sta)
 {
+    if (buf == nullptr)
+       return -1;
 
+    int i = 0; 
+    int sum = 0;
+
+    buf[PRO_HEAD1] = 0xA5;
+    buf[PRO_HEAD2] = 0x5A;
+    buf[PRO_SRC] = 0x01;
+    buf[PRO_DST] = 0x02;
+    buf[PRO_LEN] = 1 + 1;
+    buf[PRO_CMD] = cmd;
+    buf[PRO_PARA] = sta;
+    buf[PRO_CHECKSUM] = sum + buf[PRO_SRC] + buf[PRO_DST] + buf[PRO_LEN] + buf[PRO_CMD] + buf[PRO_PARA];
+    buf[PRO_TAIL] = 0xFE;
+
+    return PRO_TAIL;
 }
 
 
